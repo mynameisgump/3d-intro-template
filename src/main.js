57d55fcb9f22f0884213dc6f350702395ee589db
mynameisgump <ecrann@protonmail.com>
2025-04-23 20:18:12 -0230

    Making changes to simplify

--- src/main.js
+++ src/main.js
@@ -1,24 +1,44 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Initialize Scene Creation
+const scene = new THREE.Scene();
+const skyboxTexture = new THREE.TextureLoader().load('/public/hdr.webp');
+scene.environment = skyboxTexture;
 
-setupCounter(document.querySelector('#counter'))
+// Initialize Camera and set position
+let followTarget = null;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 30;
+
+// Initializing the Renderer and setting to correct size
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Update camera aspect ratio and renderer on resize
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Creating a mesh and adding to the scene
+const cubeGeo = new THREE.BoxGeometry(5,5,5)
+const cubeMat = new THREE.MeshBasicMaterial()
+const cube =new THREE.Mesh(cubeGeo,cubeMat);
+scene.add(cube)
+
+// Code For the Animation Loop
+function animate() {
+  renderer.render(scene, camera);
+  
+  cube.rotation.x += 0.02
+  cube.rotation.y += 0.01
+  cube.rotation.z += 0.04
+}
+renderer.setAnimationLoop(animate);
